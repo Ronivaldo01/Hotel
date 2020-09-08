545d981c65b202146ad665e88067b1d4153029b4
ronivaldo <ronivaldo01@yahoo.com.br>
2020-09-07 22:24:20 -0300

    Subindo Projeto Hotel

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+    {
+        // ...
+        public function register()
+        {
+            $this->app->bind(
+                \Auth0\Login\Contract\Auth0UserRepository::class,
+                \Auth0\Login\Repository\Auth0UserRepository::class
+            );
+        }
+    }
